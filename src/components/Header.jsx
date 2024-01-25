26ef0986bca9c43a9d0c08ce39ba6c0bab09b294
code25 <chingisenkhbaatar@gmail.com>
2024-01-25 19:31:34 +0100

    initial commit of the Header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="">
+      <div className="main-photo-container no-backdrop-filter">
+        <h1 className="name">Chingis Enkhbaatar</h1>
+      </div>
+      <section className="about-me glass-effect">
+        <h2>About me</h2>
+        <p className="about-me">
+          Dedicated and detail-oriented, aspiring to build a dynamic career in
+          web development with expertise in both front-end and back-end. Possess
+          a strong foundation in HTML, CSS, and JavaScript, coupled with
+          experience in modern frameworks such as React for front-end
+          development and Node.js for back-end development. Adept at creating
+          responsive and user-friendly web applications
+        </p>
+        <div className="icon-container">
+          <a href="tel:+123456789">
+            <img
+              className="icon"
+              src="/icons/icons8-phone-50.png"
+              alt="phone icon"
+            />
+          </a>
+          <a href="mailto:chingisenkhbaatar@gmail.com">
+            <img
+              className="icon"
+              src="/icons/icons8-email-64.png"
+              alt="email icon"
+            />
+          </a>
+          <a href="https://github.com/codetwentyfive" target="_blank">
+            <img
+              className="icon"
+              src="/icons/icons8-github-50.png"
+              alt="GitHub icon"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/chingis-enkhbaatar"
+            target="_blank"
+          >
+            <img
+              className="icon"
+              src="/icons/icons8-linkedin-48.png"
+              alt="LinkedIn icon"
+            />
+          </a>
+        </div>
+      </section>
+      <div className="draw">
+        <div className="rectangle"></div>
+        <div className="triangle"></div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
