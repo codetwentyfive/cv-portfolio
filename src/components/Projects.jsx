effddea53253458104bc6135cf4aa67db996c5ca
code25 <chingisenkhbaatar@gmail.com>
2024-02-03 01:13:14 +0100

    added cv application to projecs

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,12 +17,19 @@ const Projects = () => {
         'The project showcases my skills in layout management and asset creation, utilizing stable diffusion to generate captivating dumpling visuals. Dumpling Haven, a fictional, "possibly future" dumpling restaurant, served as the canvas for my design passion, merging aesthetics with the anticipation of culinary possibilities.',
     },
     {
-      id: 3,
+      id: 4,
       name: "Sign-Up Form",
       link: "https://github.com/codetwentyfive/Sign-up-Form",
       screenshot: "signup-screenshot.png",
       description:
-        'Discover the Buuz.com sign-up form—created using HTML, CSS, and JavaScript. It showcases a seamless and engaging user experience. Step into the realm of Buuz.com, where the blend of design and functionality transforms the sign-up process into an easy process, ensuring users effortlessly join the Buuz.com community.',
+        "Discover the Buuz.com sign-up form—created using HTML, CSS, and JavaScript. It showcases a seamless and engaging user experience. Step into the realm of Buuz.com, where the blend of design and functionality transforms the sign-up process into an easy process, ensuring users effortlessly join the Buuz.com community.",
+    },
+    {
+      id: 3,
+      name: "CV-Application",
+      link: "https://github.com/codetwentyfive/cv-application",
+      screenshot: "cv-application-screenshot.png",
+      description: "Created an easy-to-use CV-Maker.\nSee updates in real time as you make them in your CV.\nFor a modern approach to making the boring more fun",
     },
   ];
 
@@ -47,10 +54,7 @@ const Projects = () => {
                 <p className="description">{project.description}</p>
               </div>
               <div className="project-icon-container">
-                <a
-                  href={`${project.link}`}
-                  target="_blank"
-                >
+                <a href={`${project.link}`} target="_blank">
                   <img
                     className="icon"
                     src="/icons/icons8-github-50.png"
